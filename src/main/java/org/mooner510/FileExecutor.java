1e78a3f1d6210a763770ff75b3e099b3e2dfd3e6
Mooner510 <lmck510@gmail.com>
2023-07-12 19:27:39 +0900

    Update All

--- src/main/java/org/mooner510/FileExecutor.java
+++ src/main/java/org/mooner510/FileExecutor.java
@@ -1,4 +1,40 @@
 package org.mooner510;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+
 public class FileExecutor {
+    public FileExecutor(String from, String to, int level) {
+        File file = new File(to);
+        if(file.exists()) file.delete();
+
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(from));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(to))
+        ) {
+            String s;
+            while (!reader.readLine().startsWith("[HitObjects]"));
+
+            JSONArray jsonArray = new JSONArray();
+            while ((s = reader.readLine()) != null) {
+                if(s.isBlank()) continue;
+                String[] split = s.split(",");
+                JSONObject json = new JSONObject();
+                switch (split[0]) {
+                    case "64" -> json.put("key", 0);
+                    case "192" -> json.put("key", 1);
+                    case "320" -> json.put("key", 2);
+                    case "448" -> json.put("key", 3);
+                }
+                json.put("offset", Long.parseLong(split[2]));
+                jsonArray.put(json);
+            }
+            writer.write(new JSONObject().put("level", level).put("notes", jsonArray).toString());
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/org/mooner510/OSUToBeatTable.java
+++ src/main/java/org/mooner510/OSUToBeatTable.java
@@ -1,2 +1,13 @@
-package org.mooner510;public class OSUToBeatTable {
+package org.mooner510;
+
+public class OSUToBeatTable {
+
+    private static final String from = "C:\\Users\\DSM2022\\Desktop\\Projects\\Java\\OSUToBeatTable\\src\\main\\resources\\osu.txt";
+
+    private static final String to = "C:\\Users\\DSM2022\\Desktop\\Projects\\QSS\\Unity\\NoteBeat\\NewBuild\\BeatTable_Data\\Resources\\Sub\\UsagiFlap";
+    private static final String toFile = "map-Hard.json";
+
+    public static void main(String[] args) {
+        new FileExecutor(from, to + "\\" + toFile, 13);
+    }
 }
