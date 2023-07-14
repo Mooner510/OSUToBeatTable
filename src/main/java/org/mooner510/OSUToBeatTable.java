a7895c949b315c23e9654e728a49b03e6eb7bd91
Mooner510 <lmck510@gmail.com>
2023-07-14 10:01:08 +0900

    Update API

--- src/main/java/org/mooner510/OSUToBeatTable.java
+++ src/main/java/org/mooner510/OSUToBeatTable.java
@@ -4,10 +4,10 @@ public class OSUToBeatTable {
 
     private static final String from = "C:\\Users\\DSM2022\\Desktop\\Projects\\Java\\OSUToBeatTable\\src\\main\\resources\\osu.txt";
 
-    private static final String to = "C:\\Users\\DSM2022\\Desktop\\Projects\\QSS\\Unity\\NoteBeat\\NewBuild\\BeatTable_Data\\Resources\\Sub\\UsagiFlap";
-    private static final String toFile = "map-Hard.json";
+    private static final String to = "C:\\Users\\DSM2022\\Desktop\\Projects\\QSS\\Unity\\NoteBeat\\Assets\\Resources\\Maps\\Hanataba";
+    private static final String toFile = "map-Master.json";
 
     public static void main(String[] args) {
-        new FileExecutor(from, to + "\\" + toFile, 13);
+        new FileExecutor(from, to + "\\" + toFile, 18);
     }
 }
